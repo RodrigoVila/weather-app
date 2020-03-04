93a99333079388c9ac49ac262c69a233a8136c78
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2020-03-04 15:39:41 -0300

    Updates

---
+++ precache-manifest.55c7aaa0d23826f97c7481c2a7516e89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98580449928f97da2f83e96b5afedd2",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "df68b3a02982c6e2d705",
+    "url": "/weather-app/static/css/main.e501d673.chunk.css"
+  },
+  {
+    "revision": "bd59892da67ddae96e66",
+    "url": "/weather-app/static/js/2.e933b5db.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/weather-app/static/js/2.e933b5db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df68b3a02982c6e2d705",
+    "url": "/weather-app/static/js/main.bd34bde9.chunk.js"
+  },
+  {
+    "revision": "f95d929b2dc853b2601a",
+    "url": "/weather-app/static/js/runtime-main.0c118c13.js"
+  }
+]);
